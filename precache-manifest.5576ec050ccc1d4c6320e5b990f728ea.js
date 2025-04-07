7ca4c1ec98f1d36a0c5d3430a319fc6e8f7c0654
Ducthanh040402 <thanh442002@gmail.com>
2025-04-07 16:58:29 +0700

    Updates

---
+++ precache-manifest.5576ec050ccc1d4c6320e5b990f728ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde332312a13b8f3b634b61db612fe47",
+    "url": "/quiz-game/index.html"
+  },
+  {
+    "revision": "b936e28957c5adebc8cd",
+    "url": "/quiz-game/static/css/main.d53eddbd.chunk.css"
+  },
+  {
+    "revision": "8fcd258462d1ebde23d7",
+    "url": "/quiz-game/static/js/2.55b36d11.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/quiz-game/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "b936e28957c5adebc8cd",
+    "url": "/quiz-game/static/js/main.57129455.chunk.js"
+  },
+  {
+    "revision": "01c9df3a0429242f53a7",
+    "url": "/quiz-game/static/js/runtime~main.fb0f95a2.js"
+  }
+]);
